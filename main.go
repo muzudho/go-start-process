2a06adb301bb494cfc558ca3340f9a9633b0849b
むずでょ <muzudho1@gmail.com>
2021-03-02 20:07:59 +0900

    External Process Stdout

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -17,6 +19,7 @@ func main() {
 	fmt.Printf("GSP     | DefaultWorkingDirectory=%s\n", dwd)
 
 	// コマンドライン引数
+	wd := flag.String("WorkingDirectory", dwd, "Working directory path.")
 	filePath := flag.String("FilePath", dwd, "Executable file path.")
 	argumentList := flag.String("ArgumentList", "", "Parameters.")
 	flag.Parse()
@@ -24,16 +27,69 @@ func main() {
 
 	parameters := strings.Split(*argumentList, " ")
 
-	startProcess(*filePath, parameters)
+	outputFileName := filepath.Join(*wd, "go-start-process.log")
+	writeString(outputFileName, "GSP     | Start\n")
+
+	startProcess(*filePath, parameters, outputFileName)
+
+	writeString(outputFileName, "GSP     | End\n")
 }
 
-func startProcess(filePath string, parameters []string) {
-	fmt.Printf("GSP     | filePath=%s\n", filePath)
+func startProcess(exeFilePath string, parameters []string, outputFileName string) {
+	fmt.Printf("GSP     | exeFilePath=%s\n", exeFilePath)
 	fmt.Printf("GSP     | parameters=%s\n", parameters)
-	cmd := exec.Command(filePath, parameters...)
+	fmt.Printf("GSP     | outputFileName=%s\n", outputFileName)
+
+	cmd := exec.Command(exeFilePath, parameters...)
+
+	externalProcessStdout, _ := cmd.StdoutPipe()
+	defer externalProcessStdout.Close()
+
 	err := cmd.Start()
 	if err != nil {
 		panic(fmt.Errorf("cmd.Start() --> [%s]", err))
 	}
+
+	receiveExternalProcessStdout(externalProcessStdout, outputFileName)
+
 	cmd.Wait()
+}
+
+func receiveExternalProcessStdout(externalProcessStdout io.ReadCloser, outputFileName string) {
+	var buffer [1]byte // これが満たされるまで待つ。1バイト。
+
+	p := buffer[:]
+
+	for {
+		n, err := externalProcessStdout.Read(p)
+
+		if nil != err {
+			if fmt.Sprintf("%s", err) == "EOF" {
+				// End of file
+				return
+			}
+
+			panic(err)
+		}
+
+		if 0 < n {
+			bytes := p[:n]
+
+			// 思考エンジンから１文字送られてくるたび、表示。
+			writeString(outputFileName, string(bytes))
+		}
+	}
+}
+
+func writeString(fileName string, contents string) {
+	// 上書き用ファイル
+	file, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		// ログのファイル・オープン失敗
+		panic(err)
+	}
+	defer file.Close()
+
+	// 数字を書込
+	file.WriteString(contents)
 }
